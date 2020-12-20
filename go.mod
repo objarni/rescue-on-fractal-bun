1fd704df2d4d12df090c2a9b5ae85b7f3acbbb46
Olof Bjarnason <olof.bjarnason@gmail.com>
2020-12-20 15:29:54 +0100

    Play sound on A key (w.i.p)
    Also ignore "main" binaries and update dependencies

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module objarni/rescue-on-fractal-bun
 go 1.13
 
 require (
+	github.com/faiface/beep v1.0.2
 	github.com/faiface/pixel v0.10.0
 	golang.org/x/image v0.0.0-20190523035834-f03afa92d3ff
 )
--- spikes/spike2/main.go
+++ spikes/spike2/main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"fmt"
+	"github.com/faiface/beep"
+	"github.com/faiface/beep/mp3"
+	"github.com/faiface/beep/speaker"
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
 	"golang.org/x/image/colornames"
+	"log"
+	"os"
+	"time"
 )
 
 type Asd struct {
@@ -13,7 +19,34 @@ type Asd struct {
 	D bool
 }
 
+//done := make(chan bool)
+//speaker.Play(beep.Seq(streamer, beep.Callback(func() {
+//	done <- true
+//})))
+//
+//<-done
+//
+
 func run() {
+	f, err := os.Open("/usr/share/teams/resources/assets/audio/bubbles.mp3")
+	if err != nil {
+		log.Fatal(err)
+	}
+	streamer, format, err := mp3.Decode(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer streamer.Close()
+
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+
+	//done := make(chan bool)
+	//speaker.Play(beep.Seq(streamer, beep.Callback(func() {
+	//	done <- true
+	//})))
+	//
+	//<-done
+
 	cfg := pixelgl.WindowConfig{
 		Title:  "Push A, S and D to play drums!",
 		Bounds: pixel.R(0, 0, 400, 300),
@@ -23,13 +56,17 @@ func run() {
 		panic(err)
 	}
 
-	//basicAtlas := text.NewAtlas(basicfont.Face7x13, text.ASCII)
-	//basicTxt := text.New(pixel.V(100, 500), basicAtlas)
-
 	asd := Asd{false, false, false}
 
 	for !win.Closed() {
 		win.Clear(colornames.Blue)
+		if win.JustPressed(pixelgl.KeyA) {
+			speaker.Play(beep.Seq(streamer))
+		}
+		if win.JustReleased(pixelgl.KeyA) {
+			speaker.Clear()
+			streamer.Seek(0)
+		}
 		asd.A = win.Pressed(pixelgl.KeyA)
 		asd.S = win.Pressed(pixelgl.KeyS)
 		asd.D = win.Pressed(pixelgl.KeyD)
@@ -37,13 +74,6 @@ func run() {
 		win.Update()
 	}
 }
-
-//func drawHelloWorldAt(basicTxt *text.Text, pos Pos, win *pixelgl.Window) {
-//	basicTxt.Clear()
-//	basicTxt.Orig = pixel.V(pos.X, pos.Y)
-//	_, _ = fmt.Fprintln(basicTxt, "Hello, text!")
-//	basicTxt.Draw(win, pixel.IM.Scaled(basicTxt.Orig, 2))
-//}
 
 func main() {
 	pixelgl.Run(run)
