34953b254305d3f5e95c5d50a84084d6ee859c8d
Olof Bjarnason <olof.bjarnason@gmail.com>
2020-12-20 22:54:12 +0100

    Introduce ControllerKey and Scene types and play some

--- cmd/rescue/main.go
+++ cmd/rescue/main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
+	"golang.org/x/image/colornames"
+	"objarni/rescue-on-fractal-bun/internal"
 )
 
 func run() {
+
+	scene := internal.MenuScene{}
+
 	cfg := pixelgl.WindowConfig{
-		Title:  "Pixel Rocks!",
-		Bounds: pixel.R(0, 0, 1024, 768),
+		Title:  "Rescue on fractal bun (work title)",
+		Bounds: pixel.R(0, 0, 800, 600),
 	}
 	win, err := pixelgl.NewWindow(cfg)
 	if err != nil {
@@ -16,6 +21,21 @@ func run() {
 	}
 
 	for !win.Closed() {
+		win.Clear(colornames.Aliceblue)
+		// TODO: DRY - make a 'dictionary' from pixelgl key to ControllerKey
+		if win.JustPressed(pixelgl.KeyDown) {
+			scene.HandleKeyDown(internal.Down)
+		}
+		if win.JustReleased(pixelgl.KeyDown) {
+			scene.HandleKeyUp(internal.Down)
+		}
+		if win.JustPressed(pixelgl.KeyUp) {
+			scene.HandleKeyDown(internal.Up)
+		}
+		if win.JustReleased(pixelgl.KeyUp) {
+			scene.HandleKeyUp(internal.Up)
+		}
+		//scene.Render()
 		win.Update()
 	}
 }
