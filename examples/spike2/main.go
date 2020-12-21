43581247764cb45af7448068981a32e92dc8ae04
Olof Bjarnason <olof.bjarnason@gmail.com>
2020-12-21 03:57:03 +0100

    Play sounds with polyphony

--- examples/spike2/main.go
+++ examples/spike2/main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"github.com/faiface/beep"
-	"github.com/faiface/beep/mp3"
 	"github.com/faiface/beep/speaker"
+	"github.com/faiface/beep/wav"
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
 	"golang.org/x/image/colornames"
-	"log"
 	"os"
 	"time"
 )
@@ -28,51 +26,59 @@ type Asd struct {
 //
 
 func run() {
-	f, err := os.Open("/usr/share/teams/resources/assets/audio/bubbles.mp3")
-	if err != nil {
-		log.Fatal(err)
-	}
-	streamer, format, err := mp3.Decode(f)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer streamer.Close()
+	err, format, abuffer := loadWav("assets/Jump.wav")
+	failOnError(err)
+	err, format, sbuffer := loadWav("assets/InventoryCursorMoved.wav")
+	failOnError(err)
+	err, format, dbuffer := loadWav("assets/MenuPointerMoved.wav")
+	failOnError(err)
 
-	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
-
-	//done := make(chan bool)
-	//speaker.Play(beep.Seq(streamer, beep.Callback(func() {
-	//	done <- true
-	//})))
-	//
-	//<-done
+	err = speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10)) //done := make(chan bool)
+	failOnError(err)
 
 	cfg := pixelgl.WindowConfig{
 		Title:  "Push A, S and D to play drums!",
 		Bounds: pixel.R(0, 0, 400, 300),
 	}
 	win, err := pixelgl.NewWindow(cfg)
-	if err != nil {
-		panic(err)
-	}
+	failOnError(err)
 
 	asd := Asd{false, false, false}
 
 	for !win.Closed() {
 		win.Clear(colornames.Blue)
 		if win.JustPressed(pixelgl.KeyA) {
-			speaker.Play(beep.Seq(streamer))
+			speaker.Play(abuffer.Streamer(0, abuffer.Len()))
 		}
-		if win.JustReleased(pixelgl.KeyA) {
-			speaker.Clear()
-			streamer.Seek(0)
+		if win.JustPressed(pixelgl.KeyS) {
+			speaker.Play(sbuffer.Streamer(0, sbuffer.Len()))
+		}
+		if win.JustPressed(pixelgl.KeyD) {
+			speaker.Play(dbuffer.Streamer(0, dbuffer.Len()))
 		}
 		asd.A = win.Pressed(pixelgl.KeyA)
 		asd.S = win.Pressed(pixelgl.KeyS)
 		asd.D = win.Pressed(pixelgl.KeyD)
-		fmt.Println(asd)
 		win.Update()
 	}
+}
+
+func failOnError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func loadWav(wavFile string) (error, beep.Format, *beep.Buffer) {
+	file, err := os.Open(wavFile)
+	failOnError(err)
+	streamer, format, err := wav.Decode(file)
+	failOnError(err)
+	asound := beep.NewBuffer(format)
+	asound.Append(streamer)
+	err = streamer.Close()
+	failOnError(err)
+	return err, format, asound
 }
 
 func main() {
