f40df53b6e6e49e3561c74264585de3331349f7b
Olof Bjarnason <olof.bjarnason@gmail.com>
2020-12-29 10:09:43 +0100

    Play with chall 7

--- examples/challenge7/gubbe.go
+++ examples/challenge7/gubbe.go
@@ -7,13 +7,13 @@ import (
 const MAXVELOCITY = 2
 
 type Gubbe struct {
-	state        State
-	looking      Looking
-	image        Image
-	stepTillSwap int
-	pos          pixel.Vec
-	vel          pixel.Vec
-	acc          pixel.Vec
+	state   State
+	looking Looking
+	image   Image
+	counter int
+	pos     pixel.Vec
+	vel     pixel.Vec
+	acc     pixel.Vec
 }
 
 type State int
@@ -72,38 +72,36 @@ func stepGubbe(g *Gubbe, controls Controls) {
 			g.looking = Right
 			g.image = WalkRight1
 			g.acc = pixel.Vec{X: 1, Y: 0}
-			g.stepTillSwap = 10
+			g.counter = 10
 		}
 		if controls.left {
 			g.state = Walking
 			g.looking = Left
 			g.image = WalkRight1
 			g.acc = pixel.Vec{X: -1, Y: 0}
-			g.stepTillSwap = 10
+			g.counter = 0
 		}
 		g.vel = g.vel.Scaled(0.9)
 	case Walking:
-		// Switch image every step
-		g.stepTillSwap--
-		if g.stepTillSwap == 0 {
-			g.stepTillSwap = 10
+		g.counter++
+		if g.counter%10 == 0 {
 			if g.image == WalkRight1 {
 				g.image = WalkRight2
 			} else {
 				g.image = WalkRight1
 			}
 		}
-		if controls.right {
+		if controls.right && g.looking == Left {
 			g.state = Walking
 			g.looking = Right
 			g.image = WalkRight1
 			g.acc = pixel.Vec{X: 1, Y: 0}
-		} else if controls.left {
+		} else if controls.left && g.looking == Right {
 			g.state = Walking
 			g.looking = Left
 			g.image = WalkRight1
 			g.acc = pixel.Vec{X: -1, Y: 0}
-		} else {
+		} else if controls.left == controls.right {
 			g.state = Standing
 			g.image = StandingRight
 			g.acc = pixel.ZV
