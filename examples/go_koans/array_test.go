e205c68d15f2c3b919d8990dcb5f72145cf905fc
Olof Bjarnason <olof.bjarnason@gmail.com>
2021-01-06 20:34:00 +0100

    Add more array examples

--- examples/go_koans/array_test.go
+++ examples/go_koans/array_test.go
@@ -10,3 +10,25 @@ func ExampleDeclareAndInitializeIntArray() {
 	// Output:
 	// [1 2 3]
 }
+
+func ExampleLoopOverArrayIndices() {
+	ia := []int{
+		1, 2, 3,
+	}
+	for i := range ia {
+		fmt.Print(i)
+	}
+	// Output:
+	// 012
+}
+
+func ExampleLoopOverArrayValues() {
+	ia := []int{
+		1, 2, 3,
+	}
+	for _, val := range ia {
+		fmt.Print(val)
+	}
+	// Output:
+	// 123
+}
