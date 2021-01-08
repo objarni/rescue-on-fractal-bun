d60ed2141599d0cb2e9c5bcd7b14596f4805c1ce
Olof Bjarnason <olof.bjarnason@gmail.com>
2021-01-08 04:41:37 +0100

    Add experimental gamepad support

--- rescue.go
+++ rescue.go
@@ -41,6 +41,50 @@ func run() {
 			win.SetClosed(true)
 		}
 
+		if win.JoystickJustPressed(pixelgl.Joystick1, pixelgl.ButtonDpadLeft) {
+			scene = scene.HandleKeyDown(internal.Left)
+		}
+		if win.JoystickJustReleased(pixelgl.Joystick1, pixelgl.ButtonDpadLeft) {
+			scene = scene.HandleKeyUp(internal.Left)
+		}
+		if win.JoystickJustPressed(pixelgl.Joystick1, pixelgl.ButtonDpadRight) {
+			scene = scene.HandleKeyDown(internal.Right)
+		}
+		if win.JoystickJustReleased(pixelgl.Joystick1, pixelgl.ButtonDpadRight) {
+			scene = scene.HandleKeyUp(internal.Right)
+		}
+		if win.JoystickJustPressed(pixelgl.Joystick1, pixelgl.ButtonDpadUp) {
+			scene = scene.HandleKeyDown(internal.Up)
+		}
+		if win.JoystickJustReleased(pixelgl.Joystick1, pixelgl.ButtonDpadUp) {
+			scene = scene.HandleKeyUp(internal.Up)
+		}
+		if win.JoystickJustPressed(pixelgl.Joystick1, pixelgl.ButtonDpadDown) {
+			scene = scene.HandleKeyDown(internal.Down)
+		}
+		if win.JoystickJustReleased(pixelgl.Joystick1, pixelgl.ButtonDpadDown) {
+			fmt.Println(pixelgl.ButtonDpadDown)
+			scene = scene.HandleKeyUp(internal.Down)
+		}
+		if win.JoystickJustPressed(pixelgl.Joystick1, pixelgl.ButtonA) {
+			scene = scene.HandleKeyDown(internal.Jump)
+		}
+		if win.JoystickJustReleased(pixelgl.Joystick1, pixelgl.ButtonA) {
+			fmt.Println(pixelgl.ButtonDpadDown)
+			scene = scene.HandleKeyUp(internal.Jump)
+		}
+		if win.JoystickJustPressed(pixelgl.Joystick1, pixelgl.ButtonB) {
+			scene = scene.HandleKeyDown(internal.Action)
+		}
+		if win.JoystickJustReleased(pixelgl.Joystick1, pixelgl.ButtonB) {
+			fmt.Println(pixelgl.ButtonDpadDown)
+			scene = scene.HandleKeyUp(internal.Action)
+		}
+
+		//if win.JoystickPresent(pixelgl.Joystick1) {
+		//	fmt.Println("js1:", win.JoystickName(pixelgl.Joystick1))
+		//}
+
 		for key, control := range controllerMap {
 			// Hmm. Just Pressed/Released APIs is 'key repeat' at least on win - problem?
 			if win.JustPressed(key) {
