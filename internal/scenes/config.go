e73bb5739d7b55d8dfed58e7ea040a7a91a7b3ab
Olof Bjarnason <olof.bjarnason@gmail.com>
2021-01-11 22:31:37 +0100

    Add nice font
    Also make more things tweakable in MapScene

---
+++ internal/scenes/config.go
@@ -0,0 +1,33 @@
+package scenes
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"time"
+)
+
+type Config struct {
+	MapSceneBlinkSpeed             int
+	MapSceneCrosshairSpeed         float64
+	MapSceneLocationTextX          int
+	MapSceneLocationTextY          int
+	MapSceneLocCircleRadius        int
+	MapSceneCurrentLocCircleRadius int
+}
+
+func TryReadCfgFrom(filename string, defaultCfg Config) Config {
+	fmt.Println("Reading ", filename, " at ", time.Now().Format(time.Stamp))
+	byteArray, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("ReadFile error, defaulting")
+		return defaultCfg
+	}
+	var cfg = defaultCfg
+	err = json.Unmarshal(byteArray, &cfg)
+	if err != nil {
+		fmt.Println("JSON parse error, defaulting. JSON was: " + string(byteArray))
+		return defaultCfg
+	}
+	return cfg
+}
--- internal/scenes/mapscene.go
+++ internal/scenes/mapscene.go
@@ -6,10 +6,13 @@ import (
 	"github.com/faiface/pixel/imdraw"
 	"github.com/faiface/pixel/pixelgl"
 	"github.com/faiface/pixel/text"
+	"github.com/golang/freetype/truetype"
 	"golang.org/x/image/colornames"
-	"golang.org/x/image/font/basicfont"
+	"golang.org/x/image/font"
 	"image/color"
+	"io/ioutil"
 	"objarni/rescue-on-fractal-bun/internal"
+	"os"
 	"unicode"
 )
 
@@ -46,8 +49,8 @@ type Location struct {
 
 type MapScene struct {
 	cfg             *Config
-	textbox         *text.Text
 	mapImage        *pixel.Sprite
+	atlas           *text.Atlas
 	locations       []Location
 	heroPos         pixel.Vec
 	heroVel         pixel.Vec
@@ -55,9 +58,35 @@ type MapScene struct {
 	hightlightTimer int
 }
 
+func loadTTF(path string, size float64) (font.Face, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	bytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	font, err := truetype.Parse(bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return truetype.NewFace(font, &truetype.Options{
+		Size:              size,
+		GlyphCacheEntries: 1,
+	}), nil
+}
+
 func MakeMapScene(cfg *Config) *MapScene {
 	// @remember: atlas sent in to render, shared by all scenes?
-	atlas := text.NewAtlas(basicfont.Face7x13, text.RangeTable(unicode.Latin))
+	face, err := loadTTF("assets/Font.ttf", 32)
+	internal.PanicIfError(err)
+	//face := basicfont.Face7x13
+	atlas := text.NewAtlas(face, text.RangeTable(unicode.Latin), text.ASCII)
 	locs := []Location{
 		{
 			position:   pixel.Vec{X: 246, Y: 486},
@@ -74,7 +103,7 @@ func MakeMapScene(cfg *Config) *MapScene {
 	}
 	return &MapScene{
 		cfg:       cfg,
-		textbox:   text.New(pixel.V(0, 30), atlas),
+		atlas:     atlas,
 		mapImage:  internal.LoadSpriteForSure("assets/TMap.png"),
 		heroPos:   pixel.Vec{50, 50},
 		heroVel:   pixel.ZV,
@@ -128,13 +157,15 @@ func (scene *MapScene) Render(win *pixelgl.Window) {
 	var imd = imdraw.New(nil)
 	for _, loc := range scene.locations {
 		vec := loc.position
-		drawCircle(imd, colornames.Darkslateblue, vec)
+		drawCircle(imd, colornames.Darkslateblue, vec,
+			scene.cfg.MapSceneLocCircleRadius)
 	}
 	blink := scene.cfg.MapSceneBlinkSpeed
 	if scene.highlight != -1 && scene.hightlightTimer/blink%2 == 0 {
 		drawCircle(
 			imd, colornames.Green,
 			scene.locations[scene.highlight].position,
+			scene.cfg.MapSceneCurrentLocCircleRadius,
 		)
 	}
 
@@ -150,21 +181,29 @@ func (scene *MapScene) Render(win *pixelgl.Window) {
 	imd.Draw(win)
 
 	// Text
-	tb := scene.textbox
+	tb := text.New(pixel.V(
+		float64(scene.cfg.MapSceneLocationTextX),
+		float64(scene.cfg.MapSceneLocationTextY)),
+		scene.atlas)
 	tb.Clear()
 	_, _ = fmt.Fprintf(tb, "Här är du: %s\n", "Hembyn")
 	_, _ = fmt.Fprintf(tb, "Gå till? %s", "Korsningen")
-	tb.DrawColorMask(win, pixel.IM.Scaled(tb.Orig, 2), colornames.Black)
+	tb.DrawColorMask(win, pixel.IM.Scaled(tb.Orig, 1), colornames.Black)
 }
 
-func drawCircle(imd *imdraw.IMDraw, color color.RGBA, vec pixel.Vec) {
+func drawCircle(
+	imd *imdraw.IMDraw,
+	color color.RGBA,
+	vec pixel.Vec,
+	radius int,
+) {
 	imd.Color = color
 	imd.Push(v(0, 600).Add(vec.ScaledXY(v(1, -1))))
-	imd.Circle(12, 3)
+	imd.Circle(float64(radius), 3)
 }
 
 func (scene *MapScene) Tick() bool {
-	scene.heroPos = scene.heroPos.Add(scene.heroVel.Scaled(1))
+	scene.heroPos = scene.heroPos.Add(scene.heroVel.Scaled(scene.cfg.MapSceneCrosshairSpeed))
 	if scene.heroPos.X < 0 {
 		scene.heroPos.X = 0
 	}
