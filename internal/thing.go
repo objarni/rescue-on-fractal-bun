cea85e398062ce668617a4cdd671ceaf009b75db
Olof Bjarnason <olof.bjarnason@gmail.com>
2021-01-12 19:26:03 +0100

    Split up Thing interface into Animated and Controllable

--- internal/thing.go
+++ internal/thing.go
@@ -7,12 +7,17 @@ import (
 // How many milliseconds is a tick?
 const TickMs = 5
 
-type Thing interface {
+type Controllable interface {
 	HandleKeyDown(key ControlKey) Thing
-
 	HandleKeyUp(key ControlKey) Thing
+}
 
+type Animated interface {
 	Render(win *pixelgl.Window)
-
 	Tick() bool
+}
+
+type Thing interface {
+	Controllable
+	Animated
 }
