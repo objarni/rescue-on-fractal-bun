92beb25fa89a582bdfa3e8daae8c070efb0aa005
Olof Bjarnason <olof.bjarnason@gmail.com>
2021-01-14 21:20:17 +0100

    Add test for find closest location

---
+++ internal/scenes/mapscene_test.go
@@ -0,0 +1,51 @@
+package scenes
+
+import (
+	"fmt"
+	"github.com/faiface/pixel"
+)
+
+func ExampleFindClosestLocation_SinglePoint() {
+	ix := FindClosestLocation(
+		pixel.Vec{50, 50},
+		[]Location{
+			LocAt(50, 50),
+		},
+	)
+	fmt.Println("Index of closest location:", ix)
+	// Output:
+	// Index of closest location: 0
+}
+
+func LocAt(x float64, y float64) Location {
+	return Location{
+		position:   pixel.Vec{x, y},
+		discovered: false,
+	}
+}
+
+func ExampleFindClosestLocation_TwoPoints() {
+	ix := FindClosestLocation(
+		pixel.Vec{0, 0},
+		[]Location{
+			LocAt(100, 100),
+			LocAt(50, 50),
+		},
+	)
+	fmt.Println("Index of closest location:", ix)
+	// Output:
+	// Index of closest location: 1
+}
+
+func ExampleFindClosestLocation_TwoPoints_first_is_closest() {
+	ix := FindClosestLocation(
+		pixel.Vec{99, 99},
+		[]Location{
+			LocAt(100, 100),
+			LocAt(50, 50),
+		},
+	)
+	fmt.Println("Index of closest location:", ix)
+	// Output:
+	// Index of closest location: 0
+}
