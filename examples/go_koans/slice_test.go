21bf154add1f64f2c6a480010eee6d3a4def6239
Olof Bjarnason <olof.bjarnason@gmail.com>
2021-01-28 22:17:54 +0100

    Add append example

--- examples/go_koans/slice_test.go
+++ examples/go_koans/slice_test.go
@@ -19,3 +19,17 @@ func setZero(slice []int) {
 		slice[i] = 0
 	}
 }
+
+func ExampleAppend() {
+	ia := []int{
+		1, 2, 3,
+	}
+	newSlice := append(ia, 4)
+	fmt.Println(newSlice)
+	fmt.Println("len: ", len(newSlice))
+	fmt.Println("cap: ", cap(newSlice))
+	// Output:
+	// 1 2 3 4
+	// len: 4
+	// cap: 6
+}
