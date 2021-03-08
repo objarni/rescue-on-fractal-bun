de714a493a947da9420b724c11378df065284510
Olof Bjarnason <olof.bjarnason@gmail.com>
2021-03-08 07:27:28 +0100

    Group all (?) graphics op functions

--- internal/scenes/levelscene.go
+++ internal/scenes/levelscene.go
@@ -67,7 +67,7 @@ func (scene *LevelScene) Render(win *pixelgl.Window) {
 		draw.Moved(
 			scene.cameraVector(),
 			draw.OpSequence(
-				draw.ToWinOp(scene.backdropGfx()),
+				draw.ToWinOp(scene.backdropOp()),
 				draw.TileLayer(scene.level.TilepixMap, "Background"),
 				draw.TileLayer(scene.level.TilepixMap, "Platforms"),
 				draw.TileLayer(scene.level.TilepixMap, "Walls"),
@@ -84,17 +84,11 @@ func (scene *LevelScene) Render(win *pixelgl.Window) {
 	scene.drawFPS(win)
 }
 
+/* gfxOp start */
 func (scene *LevelScene) ghostOp() draw.WinOp {
-	ghostPos := v(float64(0), 200)
-	ghostSprite := draw.Moved(scene.cameraVector(), draw.Moved(ghostPos, draw.Image(scene.res.ImageMap, internal.IGhost)))
-	return ghostSprite
-}
-
-func (scene *LevelScene) drawFPS(win *pixelgl.Window) {
-	win.SetMatrix(pixel.IM)
-	tb := text.New(pixel.V(0, 0), scene.res.Atlas)
-	_, _ = fmt.Fprintf(tb, "FPS=%1.1f", scene.res.FPS)
-	tb.DrawColorMask(win, pixel.IM, colornames.Brown800)
+	return draw.Moved(scene.cameraVector(),
+		draw.Moved(v(float64(0), 200),
+			draw.Image(scene.res.ImageMap, internal.IGhost)))
 }
 
 func (scene *LevelScene) mapSymbolOp() draw.WinOp {
@@ -104,6 +98,40 @@ func (scene *LevelScene) mapSymbolOp() draw.WinOp {
 		op = draw.Color(colornames.GreenA400, op)
 	}
 	return op
+}
+
+func (scene *LevelScene) playerOp() draw.WinOp {
+	playerOp := draw.Moved(
+		scene.playerPos,
+		draw.Image(scene.res.ImageMap, internal.ITemporaryPlayerImage))
+	return playerOp
+}
+
+func (scene *LevelScene) backdropOp() draw.ImdOp {
+	widthPixels := scene.level.Width * 32
+	heightPixels := scene.level.Height * 32
+	rectangle := draw.Rectangle(draw.C(0, 0), draw.C(widthPixels, heightPixels), 0)
+	return draw.Colored(scene.level.ClearColor, rectangle)
+}
+
+func (scene *LevelScene) signPostsOp() draw.WinOp {
+	ops := []draw.WinOp{}
+	for _, mapPoint := range scene.level.SignPosts {
+		alignVec := v(0, scene.res.ImageMap[internal.ISignPost].Frame().Center().Y)
+		signPostOp := draw.Moved(mapPoint.Pos, draw.Moved(alignVec,
+			draw.Image(scene.res.ImageMap, internal.ISignPost)))
+		ops = append(ops, signPostOp)
+	}
+	return draw.OpSequence(ops...)
+}
+
+/* gfxOp stop */
+
+func (scene *LevelScene) drawFPS(win *pixelgl.Window) {
+	win.SetMatrix(pixel.IM)
+	tb := text.New(pixel.V(0, 0), scene.res.Atlas)
+	_, _ = fmt.Fprintf(tb, "FPS=%1.1f", scene.res.FPS)
+	tb.DrawColorMask(win, pixel.IM, colornames.Brown800)
 }
 
 func (scene *LevelScene) isMapSignClose() bool {
@@ -123,32 +151,12 @@ func (scene *LevelScene) closestMapSign() internal.SignPost {
 	return scene.level.SignPosts[internal.ClosestPoint(scene.playerPos, points)]
 }
 
-func (scene *LevelScene) cameraMatrix() pixel.Matrix {
-	reversed := scene.cameraVector()
-	cameraMatrix := pixel.IM.Moved(reversed)
-	return cameraMatrix
-}
-
 func (scene *LevelScene) cameraVector() pixel.Vec {
 	halfScreen := v(internal.ScreenWidth/2, internal.ScreenHeight/2)
 	playerHead := v(0, internal.PlayerHeight)
 	cam := scene.playerPos.Sub(halfScreen).Add(playerHead)
 	reversed := cam.Scaled(-1)
 	return reversed
-}
-
-func (scene *LevelScene) playerOp() draw.WinOp {
-	playerOp := draw.Moved(
-		scene.playerPos,
-		draw.Image(scene.res.ImageMap, internal.ITemporaryPlayerImage))
-	return playerOp
-}
-
-func (scene *LevelScene) backdropGfx() draw.ImdOp {
-	widthPixels := scene.level.Width * 32
-	heightPixels := scene.level.Height * 32
-	rectangle := draw.Rectangle(draw.C(0, 0), draw.C(widthPixels, heightPixels), 0)
-	return draw.Colored(scene.level.ClearColor, rectangle)
 }
 
 func (scene *LevelScene) Tick() bool {
@@ -159,17 +167,6 @@ func (scene *LevelScene) Tick() bool {
 		scene.playerPos = scene.playerPos.Add(v(scene.cfg.LevelSceneMoveSpeed, 0))
 	}
 	return true
-}
-
-func (scene *LevelScene) signPostsOp() draw.WinOp {
-	ops := []draw.WinOp{}
-	for _, mapPoint := range scene.level.SignPosts {
-		alignVec := v(0, scene.res.ImageMap[internal.ISignPost].Frame().Center().Y)
-		signPostOp := draw.Moved(mapPoint.Pos, draw.Moved(alignVec,
-			draw.Image(scene.res.ImageMap, internal.ISignPost)))
-		ops = append(ops, signPostOp)
-	}
-	return draw.OpSequence(ops...)
 }
 
 /*
