f39309dbaf93f4880d80e1eb54ff1cc5cb6a025f
Olof Bjarnason <olof.bjarnason@gmail.com>
2021-03-10 21:25:45 +0100

    WIP clipper

--- go.mod
+++ go.mod
@@ -8,13 +8,12 @@ require (
 	github.com/faiface/beep v1.0.2
 	github.com/faiface/pixel v0.10.0
 	github.com/g4s8/hexcolor v0.0.0-20190127080027-f2cd1c90f5c4
-	github.com/go-gl/gl v0.0.0-20190320180904-bf2b1f2f34d7
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
-	github.com/smartystreets/goconvey v1.6.4 // indirect
+	github.com/gopherjs/gopherjs v0.0.0-20181017120253-0766667cb4d1 // indirect
+	github.com/lucasb-eyer/go-colorful v0.0.0-20181028223441-12d3b2882a08
 	github.com/stretchr/testify v1.6.1 // indirect
 	golang.org/x/exp v0.0.0-20180710024300-14dda7b62fcd
 	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8
-	golang.org/x/tools v0.0.0-20190328211700-ab21143f2384
 )
 
 replace github.com/bcvery1/tilepix => /home/olof/repos/github/oss/tilepix
--- tests/clipper_test.go
+++ tests/clipper_test.go
@@ -2,21 +2,71 @@ package tests
 
 import (
 	"fmt"
+	"github.com/lucasb-eyer/go-colorful"
+	"image"
+	"image/color"
+	"image/png"
+	"objarni/rescue-on-fractal-bun/internal"
+	"os"
 )
 
 func Example_clipObjectWithBlackBorder() {
-	clip("../testdata/object_with_black_border.png", "bject_with_black_border_clipped.png")
+	clip("../testdata/object_with_black_border.png", "object_with_black_border_clipped.png")
 	// Output:
 	// Clipping 'object_with_black_border.png'.
-	// Dimensions: 50x50
+	// Dimensions: 517x407
 	// 550 pixels clipped, 1550 left.
 	// Saving output to 'object_with_black_border_clipped.png'.
 }
 
+func LoadImageForSure(path string) image.Image {
+	file, err := os.Open(path)
+	internal.PanicIfError(err)
+	img, _, err := image.Decode(file)
+	internal.PanicIfError(err)
+	return img
+}
+
 func clip(toClip string, saveTo string) {
 	fmt.Println("Clipping 'object_with_black_border.png'.")
-	var width int = 51
-	fmt.Printf("Dimensions: %vx50\n", width)
+	img := LoadImageForSure(toClip)
+	var width = img.Bounds().Dx()
+	var height = img.Bounds().Dy()
+	clippedImage := image.NewRGBA(img.Bounds())
+	for x := 0; x < width; x++ {
+		firstBlackX := -1
+		for y := 0; y < height; y++ {
+			srcColor := img.At(x, y)
+			src, _ := colorful.MakeColor(srcColor)
+			black := colorful.Color{
+				R: 0,
+				G: 0,
+				B: 0,
+			}
+			var alpha uint8 = 0
+			dist := src.DistanceCIE76(black)
+			//fmt.Println(dist)
+			if dist < 0.5 {
+				if firstBlackX == -1 {
+					firstBlackX = x
+				}
+				alpha = 255
+			}
+			r, g, b := uint8(src.R), uint8(src.G), uint8(src.B)
+			clippedImage.Set(x, y, color.RGBA{r, g, b, alpha})
+		}
+	}
+	fmt.Printf("Dimensions: %vx%v\n", width, height)
 	fmt.Println("550 pixels clipped, 1550 left.")
-	fmt.Println("Saving output to 'object_with_black_border_clipped.png'.")
+	fmt.Printf("Saving output to '%v'.\n", saveTo)
+	SaveImage(clippedImage, saveTo)
+}
+
+func SaveImage(image *image.RGBA, path string) {
+	f, err := os.Create(path)
+	internal.PanicIfError(err)
+	err = png.Encode(f, image)
+	internal.PanicIfError(err)
+	err = f.Close()
+	internal.PanicIfError(err)
 }
