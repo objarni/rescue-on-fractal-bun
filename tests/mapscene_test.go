4f767eb790d3c9fc2b07afd7788ab51e8c8f932d
Olof Bjarnason <olof.bjarnason@gmail.com>
2021-03-24 18:23:04 +0100

    Move TestMain and BCompare to a new main_test file

--- tests/mapscene_test.go
+++ tests/mapscene_test.go
@@ -5,16 +5,8 @@ import (
 	"github.com/faiface/pixel"
 	"objarni/rescue-on-fractal-bun/internal"
 	"objarni/rescue-on-fractal-bun/internal/scenes"
-	"os"
 	"testing"
-	"time"
 )
-
-func TestMain(m *testing.M) {
-	r := approvals.UseReporter(NewBCompare())
-	defer r.Close()
-	os.Exit(m.Run())
-}
 
 func Test_initialRender(t *testing.T) {
 	cfg := scenes.TryReadCfgFrom("../"+internal.ConfigFile, scenes.Config{})
@@ -23,11 +15,5 @@ func Test_initialRender(t *testing.T) {
 	}
 	mapScene := scenes.MakeMapScene(&cfg, &res, "Skogen")
 	op := mapScene.MapSceneWinOp()
-
 	approvals.VerifyString(t, op.String())
-	duration, err := time.ParseDuration("3500ms")
-	if err != nil {
-		panic(err)
-	}
-	time.Sleep(duration)
 }
