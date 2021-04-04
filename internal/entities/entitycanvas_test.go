ef62e99e8e97a02e2b8669f02171ddf1b58625d5
Olof Bjarnason <olof.bjarnason@gmail.com>
2021-04-04 22:34:13 +0200

    Move entity tests to entity folder

--- internal/entities/entitycanvas_test.go
+++ internal/entities/entitycanvas_test.go
@@ -1,19 +1,19 @@
-package tests
+package entities
 
 import (
 	approvals "github.com/approvals/go-approval-tests"
 	"github.com/faiface/pixel"
-	"objarni/rescue-on-fractal-bun/internal/entities"
+	"objarni/rescue-on-fractal-bun/tests"
 	"testing"
 )
 
 func Test_1_Entity_1_EventBox_1_overlap(t *testing.T) {
-	entityCanvas := entities.MakeEntityCanvas()
-	entityCanvas.AddEntityHitBox(entities.EntityHitBox{Entity: 1, HitBox: pixel.Rect{
+	entityCanvas := MakeEntityCanvas()
+	entityCanvas.AddEntityHitBox(EntityHitBox{Entity: 1, HitBox: pixel.Rect{
 		Min: pixel.Vec{0, 0},
 		Max: pixel.Vec{10, 10},
 	}})
-	entityCanvas.AddEventBox(entities.EventBox{Event: "ACTION", Box: pixel.Rect{
+	entityCanvas.AddEventBox(EventBox{Event: "ACTION", Box: pixel.Rect{
 		Min: pixel.Vec{5, 5},
 		Max: pixel.Vec{6, 6},
 	}})
@@ -24,16 +24,16 @@ func Test_1_Entity_1_EventBox_1_overlap(t *testing.T) {
 }
 
 func init() {
-	approvals.UseReporter(ReportWithMeld())
+	approvals.UseReporter(tests.ReportWithMeld())
 }
 
 func Test_1_Entity_1_EventBox_no_overlapping(t *testing.T) {
-	entityCanvas := entities.MakeEntityCanvas()
-	entityCanvas.AddEntityHitBox(entities.EntityHitBox{Entity: 1, HitBox: pixel.Rect{
+	entityCanvas := MakeEntityCanvas()
+	entityCanvas.AddEntityHitBox(EntityHitBox{Entity: 1, HitBox: pixel.Rect{
 		Min: pixel.Vec{0, 0},
 		Max: pixel.Vec{10, 10},
 	}})
-	entityCanvas.AddEventBox(entities.EventBox{Event: "ACTION", Box: pixel.Rect{
+	entityCanvas.AddEventBox(EventBox{Event: "ACTION", Box: pixel.Rect{
 		Min: pixel.Vec{55, 55},
 		Max: pixel.Vec{76, 76},
 	}})
@@ -44,16 +44,16 @@ func Test_1_Entity_1_EventBox_no_overlapping(t *testing.T) {
 }
 
 func Test_2_Entities_1_EventBox_1_overlap(t *testing.T) {
-	entityCanvas := entities.MakeEntityCanvas()
-	entityCanvas.AddEntityHitBox(entities.EntityHitBox{Entity: 1, HitBox: pixel.Rect{
+	entityCanvas := MakeEntityCanvas()
+	entityCanvas.AddEntityHitBox(EntityHitBox{Entity: 1, HitBox: pixel.Rect{
 		Min: pixel.Vec{0, 0},
 		Max: pixel.Vec{10, 10},
 	}})
-	entityCanvas.AddEntityHitBox(entities.EntityHitBox{Entity: 2, HitBox: pixel.Rect{
+	entityCanvas.AddEntityHitBox(EntityHitBox{Entity: 2, HitBox: pixel.Rect{
 		Min: pixel.Vec{20, 20},
 		Max: pixel.Vec{30, 30},
 	}})
-	entityCanvas.AddEventBox(entities.EventBox{Event: "BOOM", Box: pixel.Rect{
+	entityCanvas.AddEventBox(EventBox{Event: "BOOM", Box: pixel.Rect{
 		Min: pixel.Vec{25, 25},
 		Max: pixel.Vec{26, 26},
 	}})
@@ -64,16 +64,16 @@ func Test_2_Entities_1_EventBox_1_overlap(t *testing.T) {
 }
 
 func Test_2_Entities_1_EventBox_2_overlaps(t *testing.T) {
-	entityCanvas := entities.MakeEntityCanvas()
-	entityCanvas.AddEntityHitBox(entities.EntityHitBox{Entity: 1, HitBox: pixel.Rect{
+	entityCanvas := MakeEntityCanvas()
+	entityCanvas.AddEntityHitBox(EntityHitBox{Entity: 1, HitBox: pixel.Rect{
 		Min: pixel.Vec{20, 20},
 		Max: pixel.Vec{30, 30},
 	}})
-	entityCanvas.AddEntityHitBox(entities.EntityHitBox{Entity: 2, HitBox: pixel.Rect{
+	entityCanvas.AddEntityHitBox(EntityHitBox{Entity: 2, HitBox: pixel.Rect{
 		Min: pixel.Vec{30, 20},
 		Max: pixel.Vec{40, 30},
 	}})
-	entityCanvas.AddEventBox(entities.EventBox{Event: "BOOM", Box: pixel.Rect{
+	entityCanvas.AddEventBox(EventBox{Event: "BOOM", Box: pixel.Rect{
 		Min: pixel.Vec{0, 0},
 		Max: pixel.Vec{100, 100},
 	}})
@@ -84,16 +84,16 @@ func Test_2_Entities_1_EventBox_2_overlaps(t *testing.T) {
 }
 
 func Test_1_Entity_2_EventBoxes_2_overlaps(t *testing.T) {
-	entityCanvas := entities.MakeEntityCanvas()
-	entityCanvas.AddEntityHitBox(entities.EntityHitBox{Entity: 1, HitBox: pixel.Rect{
+	entityCanvas := MakeEntityCanvas()
+	entityCanvas.AddEntityHitBox(EntityHitBox{Entity: 1, HitBox: pixel.Rect{
 		Min: pixel.Vec{20, 20},
 		Max: pixel.Vec{30, 30},
 	}})
-	entityCanvas.AddEventBox(entities.EventBox{Event: "BOOM1", Box: pixel.Rect{
+	entityCanvas.AddEventBox(EventBox{Event: "BOOM1", Box: pixel.Rect{
 		Min: pixel.Vec{21, 21},
 		Max: pixel.Vec{22, 22},
 	}})
-	entityCanvas.AddEventBox(entities.EventBox{Event: "BOOM2", Box: pixel.Rect{
+	entityCanvas.AddEventBox(EventBox{Event: "BOOM2", Box: pixel.Rect{
 		Min: pixel.Vec{29, 29},
 		Max: pixel.Vec{30, 30},
 	}})
