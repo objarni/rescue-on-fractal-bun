2f9d317d5cf302639edadb2fff909a62a82c8c1d
Olof Bjarnason <olof.bjarnason@gmail.com>
2021-04-10 10:48:37 +0200

    Switch GhostForest to use rectangles for Entitites

--- examples/tiled_test/TileSet.tsx
+++ examples/tiled_test/TileSet.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.2" name="TileSetImage" tilewidth="32" tileheight="32" tilecount="162" columns="18">
- <image source="TileSetImage.png" width="600" height="300"/>
+<tileset version="1.4" tiledversion="2021.01.13" name="TileSetImage" tilewidth="32" tileheight="32" tilecount="200" columns="20">
+ <image source="TileSetImage.png" width="640" height="320"/>
 </tileset>
