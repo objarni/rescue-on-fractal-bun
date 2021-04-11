4f40b6b4d09ea3b1158c8a2eafa5863f976f5998
Olof Bjarnason <olof.bjarnason@gmail.com>
2021-04-11 10:27:13 +0200

    Ghosts turn when facing left

--- internal/entities/ghost.go
+++ internal/entities/ghost.go
@@ -1,17 +1,17 @@
 package entities
 
 import (
-	"github.com/faiface/pixel"
+	px "github.com/faiface/pixel"
 	"math"
 	"objarni/rescue-on-fractal-bun/internal"
-	"objarni/rescue-on-fractal-bun/internal/draw"
+	d "objarni/rescue-on-fractal-bun/internal/draw"
 )
 
 const ghostWidth = 50
 const ghostHeight = 125
 
 type Ghost struct {
-	pos         pixel.Vec
+	pos         px.Vec
 	baseLine    float64
 	dirX        float64
 	minX, maxX  float64
@@ -22,10 +22,10 @@ func (ghost Ghost) Handle(_ EventBox) Entity {
 	return ghost
 }
 
-func (ghost Ghost) HitBox() pixel.Rect {
-	min := ghost.pos.Add(pixel.V(-ghostWidth/2, 0))
-	max := ghost.pos.Add(pixel.V(ghostWidth/2, ghostHeight))
-	return pixel.Rect{Min: min, Max: max}
+func (ghost Ghost) HitBox() px.Rect {
+	min := ghost.pos.Add(px.V(-ghostWidth/2, 0))
+	max := ghost.pos.Add(px.V(ghostWidth/2, ghostHeight))
+	return px.Rect{Min: min, Max: max}
 }
 
 func (ghost Ghost) Tick(gameTimeMs float64, receiver EventBoxReceiver) Entity {
@@ -45,12 +45,16 @@ func (ghost Ghost) Tick(gameTimeMs float64, receiver EventBoxReceiver) Entity {
 	return ghost
 }
 
-func (ghost Ghost) GfxOp(imageMap *internal.ImageMap) draw.WinOp {
-	return draw.Moved(ghost.pos.Add(pixel.V(0, ghostHeight/2)),
-		draw.Image(*imageMap, internal.IGhost))
+func (ghost Ghost) GfxOp(imageMap *internal.ImageMap) d.WinOp {
+	image := d.Image(*imageMap, internal.IGhost)
+	if ghost.dirX < 0 {
+		image = d.Mirrored(image)
+	}
+	return d.Moved(ghost.pos.Add(px.V(0, ghostHeight/2)),
+		image)
 }
 
-func MakeGhost(area pixel.Rect) Entity {
+func MakeGhost(area px.Rect) Entity {
 	startPos := area.Center()
 	return Ghost{
 		pos:         startPos,
